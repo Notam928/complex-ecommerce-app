69a7f9f51ab1606918d693767869b5ada8b014a2
Jules <adonsijules9@gmail.com>
2021-01-13 19:32:40 +0100

    Adding product display and detail view features

--- base/settings/base.py
+++ base/settings/base.py
@@ -108,4 +108,3 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-DEBUG_COLLECTSTATIC=1
