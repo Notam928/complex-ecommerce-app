6c71cb8e3b2b626f0b0b04436550fa5bb0b43cb9
Jules <adonsijules9@gmail.com>
2021-01-13 19:30:35 +0100

    Adding product display and detail view features

--- base/settings/base.py
+++ base/settings/base.py
@@ -105,3 +105,7 @@ LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+DEBUG_COLLECTSTATIC=1
